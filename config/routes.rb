7c88d9eb1885cd9031bc2535ade5d6b9306da9e7
Paul Devanney <pauldevanney92@gmail.com>
2024-09-24 18:52:00 +0100

    Implement Wicinimous CRUD App

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "/articles", to: "articles#index"
+
+  get "/articles/new", to: "articles#new"
+  post "/articles", to: "articles#create"
+
+  get "/articles/:id", to: "articles#show", as: :article
+
+  get "/articles/:id/edit", to: "articles#edit", as: :articles_edit
+  patch "/articles/:id", to: "articles#update"
+
+  delete "/articles/:id", to: "articles#destroy"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
